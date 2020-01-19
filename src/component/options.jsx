41365a8ac87ce24cac718fd667badd17ac15e839
Garout <emil.stromdahl@live.com>
2020-01-19 11:22:49 +0100

    added options window

---
+++ src/component/options.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+const electron = window.require("electron");
+
+export default class home extends Component {
+    render() {
+        return (
+            <div id="main" className="wrapper-home">
+                <h1>Options</h1>
+            </div>
+        );
+    }
+}
